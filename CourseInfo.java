7593a639d99a534d0a733c4aa0735898a69ad54a
John Noah Lukonis <johnnoahlukonis@go.rmc.edu>
2026-05-06 14:27:40 -0400

    Update Number of Students and Classtime

---
+++ CourseInfo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CourseInfo{
+
+	public static void main(String[] args) {
+		System.out.println("Course Name: Object-Oriented Programming");
+		System.out.println("Instructor: Dr. Elouni");
+		System.out.println("Semester: Spring 2026");
+		System.out.println("Credits: 4");
+		System.out.println("Classroom: COP 204");
+		System.out.println("Classtime: MW 1:10-2:40   TR 1:20-2:50");
+		System.out.println("Number of Students: 25");
+	}
+}
