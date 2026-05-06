ee0ae4dcac2cb88f2f469f0783030b017ca57f49
John Noah Lukonis <johnnoahlukonis@go.rmc.edu>
2026-05-06 14:11:34 -0400

    Initial Version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CourseInfo{
+
+	public static void main(String[] args) {
+		System.out.println("Course Name: Object-Oriented Programming");
+		System.out.println("Instructor: Dr. Elouni");
+		System.out.println("Semester: Spring 2026");
+		System.out.println("Credits: 4");
+		System.out.println("Classroom: COP 204");
+	}
+}
