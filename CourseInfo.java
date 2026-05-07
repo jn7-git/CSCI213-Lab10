c944c244cc736287bed38bb21a2738e794e66782
John Noah Lukonis <johnnoahlukonis@go.rmc.edu>
2026-05-07 13:47:15 -0400

    trial

--- CourseInfo.java
+++ CourseInfo.java
@@ -8,10 +8,10 @@ public class CourseInfo{
 		System.out.println("Semester: Spring 2026");
 		System.out.println("Credits: 4");
 		System.out.println("Classroom: COP 204");
-<<<<<<< HEAD
-=======
+
+
 		System.out.println("Classtime: MW 1:10-2:40   TR 1:20-2:50");
 		System.out.println("Number of Students: 25");
->>>>>>> lukonis
+
 	}
 }
